59c5d52c4314e10584e8c0d435e221b6e09c8c3e
neglectPanda <mypacc006@gmail.com>
2024-10-04 10:52:16 +0800

    Exercise 5.2

---
+++ Lab Exercise 4/LE5.2.1/barroEl_LE5_21.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main () {
+
+    int i, j;
+    int rows = 0, columns = 0;
+    int validInput;
+
+    while (rows <= 0) {
+        printf("Enter the number of rows: ");
+        validInput = scanf("%d", &rows);
+
+        if (validInput != 1) {
+            while (getchar() != '\n'); // discard invalid input
+            printf("Invalid input. Must be a positive integer.\n");
+            rows = 0; // reset rows to loop again
+        } else if (rows <= 0) {
+            printf("Invalid input. Must be a positive integer.\n");
+        }
+    }
+
+    while (columns <= 0) {
+        printf("Enter the number of columns: ");
+        validInput = scanf("%d", &columns);
+
+        // Clear the input buffer if scanf fails
+        if (validInput != 1) {
+            while (getchar() != '\n'); // discard invalid input
+            printf("Invalid input. Must be a positive integer.\n");
+            columns = 0; // reset columns to loop again
+        } else if (columns <= 0) {
+            printf("Invalid input. Must be a positive integer.\n");
+        }
+    }
+
+    printf("=======================================================\n");
+
+    for(i = 1; i <= rows; i++) {
+        for(j = 1; j <= columns; j++) {
+            printf("%d\t ", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
