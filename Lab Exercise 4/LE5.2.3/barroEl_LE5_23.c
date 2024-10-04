ff386f1b3f73d983ff9425e5bd1b95b2f060a4df
neglectPanda <mypacc006@gmail.com>
2024-10-04 19:53:39 +0800

    Test

--- Lab Exercise 4/LE5.2.3/barroEl_LE5_23.c
+++ Lab Exercise 4/LE5.2.3/barroEl_LE5_23.c
@@ -1,10 +1,34 @@
+/*
+============================================================================
+ FILE        : barroEl_LE5_23.c
+ AUTHOR      : Ellaine Joyce A. Barro
+ DESCRIPTION : Fibonacci Series
+ COPYRIGHT   : created
+ REVISION HISTORY
+ Date:               By: 	                        Description:
+ 10/04/24	         Ellaine Joyce A. Barro         Made the entire program
+============================================================================
+*/
+
 #include <stdio.h>
 
+/*
+============================================================================
+ FUNCTION    : main
+ DESCRIPTION : Given the limit, display the fibonacci series
+ ARGUMENTS   : none
+ RETURNS     : int
+ 		       Successful code execution with no errors
+===========================================================================
+*/
+
 int main() {
+    //INITIALIZE variables
     int firstNum = 1, secondNum = 1, nextNum, i;
     int limit;
     char charInputCheck;
 
+    //GET limit
     printf("Enter limit: ");
 
     while(scanf("%d", &limit) != 1 || limit < 1 ) {
@@ -16,16 +40,22 @@ int main() {
 
     printf("FIBONACCI SEQUENCE\n");
 
+    //if limit is 1 then it should just return 1
     if(limit == 1) {
         printf("%d\n", firstNum);
         return 0;
     }
 
+    //if the limit is not 1, then display the first and second number which is 1
     printf("%d %d ", firstNum, secondNum);
 
+    //start indexing at 3 (1, 1, ...)
     for(i = 3; i<= limit; i++) {
-        nextNum = firstNum + secondNum;
+        //COMPUTE nextNum to be the sum of firstNum and secondNum
+        //nextNum = firstNum + secondNum;
+        //DISPLAY the next number
         printf("%d ", nextNum);
+        //UPDATE firstNum and secondNum for the next iteration
         firstNum = secondNum;
         secondNum = nextNum;
     }
