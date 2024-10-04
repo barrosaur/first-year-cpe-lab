c3beaf2e7564f0c77206a1ff528c104314bf2449
neglectPanda <mypacc006@gmail.com>
2024-10-04 19:49:47 +0800

    Added full documentation (formal and how i understood it)

--- Lab Exercise 4/LE5.2.2/barroEl_LE5_22.c
+++ Lab Exercise 4/LE5.2.2/barroEl_LE5_22.c
@@ -1,16 +1,39 @@
+/*
+============================================================================
+ FILE        : barroEl_LE5_22.c
+ AUTHOR      : Ellaine Joyce A. Barro
+ DESCRIPTION : Triangle Number
+ COPYRIGHT   : created
+ REVISION HISTORY
+ Date:               By: 	                        Description:
+ 10/04/24	         Ellaine Joyce A. Barro         Made the entire program
+============================================================================
+*/
+
 #include <stdio.h>
+
+/*
+============================================================================
+ FUNCTION    : main
+ DESCRIPTION : Displays triangle of numbers from 1 to num
+ ARGUMENTS   : none
+ RETURNS     : int
+ 		       Successful code execution with no errors
+===========================================================================
+*/
 
 int main() {
     int i, j;
     int num;
 
+    //GET num
     printf("Enter num: ");
     scanf("%d", &num);
 
     //for every row
     for(i = 1; i <= num; i++) {
 
-        //kani siya spaces before numbers
+        //the spaces before numbers
         for(j = i; j < num; j++) {
             printf("  ");
         }
@@ -20,7 +43,7 @@ int main() {
             printf("%d ", j);
         }
 
-        //decreasing nums like the ones on the sides 
+        //decreasing nums on the sides 
         for(j = i - 1; j >= 1; j--) {
             printf("%d ", j);
         }
