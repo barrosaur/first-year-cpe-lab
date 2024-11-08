717ec4a7761a6feff156cbcba68b072b4eba5960
neglectPanda <mypacc006@gmail.com>
2024-11-08 18:13:50 +0800

    finalized

--- Lab Exercise 7/LE7.2/barroEl_LE7_12.c
+++ Lab Exercise 7/LE7.2/barroEl_LE7_12.c
@@ -1,21 +1,40 @@
+/*
+============================================================================
+ FILE        : barroEl_LE7_12.c
+ AUTHOR      : Ellaine Joyce A. Barro
+ DESCRIPTION : Prompts user for a string and counts the consonants and vowels in the string
+ COPYRIGHT   : created
+ REVISION HISTORY
+ Date:               By: 	                    Description:
+ 11/08/24	         Ellaine Joyce A. Barro     Made the entire program
+============================================================================
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
-int checkCharacter(char ch);
+int checkChar(char ch);
 
+/*
+============================================================================
+ FUNCTION    : main
+ DESCRIPTION : Displays the number of consonants and vowels in the inputted string by the user
+ ARGUMENTS   : void
+ RETURNS     : int - Successful Program Execution
+===========================================================================
+*/
 int main() {
     char str[100];
     int i, length, result;
     int vowelCount = 0, consonantCount = 0;
-    
+
     printf("Enter a string: ");
     gets(str);
 
     length = strlen(str);
 
     for(i = 0; i < length; i++) {
-        result = checkCharacter(str[i]);
+        result = checkChar(str[i]);
 
         if(result == 1) {
             vowelCount++;
@@ -24,20 +43,30 @@ int main() {
         }
     }
 
-    printf("Vowel count: %d\n", vowelCount);
-    printf("Consonant count: %d\n", consonantCount);
+    printf("\n==================================================================\n");
+
+    printf("Consonant Count   : %d\n", consonantCount);
+    printf("Vowel Count       : %d\n", vowelCount);
 
     return 0;
 }
 
-int checkCharacter(char ch) {
+/*
+============================================================================
+ FUNCTION    : checkChar
+ DESCRIPTION : Checks if characters in a string inputted by the user are vowels, consonants, or neither
+ ARGUMENTS   : char ch   - character in the string inputted by the user
+ RETURNS     : int flag  - 0, 1, 2 representing neither, vowel, or consonant respectively
+===========================================================================
+*/
+int checkChar(char ch) {
     int flag;
     bool isVowel = (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U');
-    bool isLetter = ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
+    bool isConsonant = ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
 
-    if(isVowel) {
+    if (isVowel) {
         flag = 1;
-    } else if (isLetter) {
+    } else if(isConsonant) {
         flag = 2;
     } else {
         flag = 0;
