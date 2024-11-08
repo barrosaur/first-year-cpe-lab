083538f57ff6a33493059425ce4b3f33bdb54e98
neglectPanda <mypacc006@gmail.com>
2024-11-08 21:18:50 +0800

    finalized

--- Lab Exercise 7/LE7.3/barroEl_LE7_13.c
+++ Lab Exercise 7/LE7.3/barroEl_LE7_13.c
@@ -2,64 +2,62 @@
 ============================================================================
  FILE        : barroEl_LE7_13.c
  AUTHOR      : Ellaine Joyce A. Barro
- DESCRIPTION : Given 5 strings, it displays the longest string
+ DESCRIPTION : Determines the longest of the 5 strings given by the user
  COPYRIGHT   : created
  REVISION HISTORY
  Date:               By: 	                    Description:
- 11/07/24	         Ellaine Joyce A. Barro     Made the entire program
+ 11/08/24	         Ellaine Joyce A. Barro     Made the entire program
 ============================================================================
 */
-
 #include <stdio.h>
 #include <string.h>
 
-int findMaxLength(char str[], char longestStr[], int currentMaxLength);
-void printLongestStr(char longestStr[], int maxLength);
+int checkAndUpdateLongest(char currentStr[], char longestStr[], int maxLength);
 
 /*
 ============================================================================
  FUNCTION    : main
- DESCRIPTION : Displays the longest string and its number of characters
+ DESCRIPTION : Prompts user for 5 strings and displays output
  ARGUMENTS   : void
  RETURNS     : int - Successful Program Execution
 ===========================================================================
 */
 int main() {
-    char str[100];
-    char longestStr[100];
+    char string[5][100];
+    char longestStr[100] = "";
     int maxLength = 0;
-    int i;
 
-    for (i = 1; i <= 5; i++) {
-        printf("Enter String %d: ", i);
-        gets(str);
+    printf("Enter 5 strings.\n");
 
-        maxLength = findMaxLength(str, longestStr, maxLength);
+    for(int i = 0; i < 5; i++) {
+        printf("Enter String %d: ", i+1);
+        gets(string[i]);
+
+        maxLength = checkAndUpdateLongest(string[i], longestStr, maxLength);
     }
 
-    printLongestStr(longestStr, maxLength);
+    printf("=======================================================================\n");
+    printf("The longest string is \"%s\" with %d characters.\n", longestStr, maxLength);
 
     return 0;
 }
 
 /*
 ============================================================================
- FUNCTION    : findMaxLength
- DESCRIPTION : Finds the string with the maxiumum number of characters
- ARGUMENTS   : char str[] - string entered by the user
-               char longestStr[100] - longest string
-               int currentMaxLength - the number of characters of the longest string
- RETURNS     : int currentMaxLength - the string and the number of its characters
+ FUNCTION    : checkAndUpdateLongest
+ DESCRIPTION : Determines and updates the longest string and its length
+ ARGUMENTS   : char currentStr[]   - current string iteration of the 5 strings
+               char longestStr[]   - longest string
+               int maxLength       - length of the longest string
+ RETURNS     : int maxLength       - number of characters in the longest string
 ===========================================================================
 */
-int findMaxLength(char str[], char longestStr[], int currentMaxLength) {
-    if(strlen(str) > currentMaxLength) {
-        strcpy(longestStr, str);
-        return strlen(str);
+int checkAndUpdateLongest(char currentStr[], char longestStr[], int maxLength) {
+    int length = strlen(currentStr);
+    if(length > maxLength) {
+        maxLength = length;
+        strcpy(longestStr, currentStr);
     }
-    return currentMaxLength;
-}
 
-void printLongestStr(char longestStr[], int maxLength) {
-    printf("The longest string is %s with %d characters.\n", longestStr, maxLength);
+    return maxLength;
 }
