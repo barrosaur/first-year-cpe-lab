b08f45f0a5d3f04111a534185a8067936828e07a
neglectPanda <mypacc006@gmail.com>
2024-12-03 18:35:32 +0800

    CRUDdin

--- personal activity/CRUD.c
+++ personal activity/CRUD.c
@@ -12,18 +12,20 @@ typedef struct student {
 } STUDENT;
 
 void displayMenu(char mainMenuOptions[][50], int size);
+void addStudent(STUDENT stud[], int *studentCount, int maxStudent);
+void displayList(STUDENT stud[], int studentCount);
 
 int main() {
-    STUDENT student;
+    STUDENT student[10];
     int choice = 0;
     // char c;
     bool condition = true;
     char mainMenuOptions[5][50] = {"Add a student.", "Update student information.", "Delete student information.", "Display Student Information.", "Exit."};
-    int menuCount = 5;
+    int menuCount = 5, studentSize = 10, studentCount = 0;
 
     while(condition) {
+        printf("=============================================================================================\n");
         printf("Welcome to the Portal!\n");
-        printf("=================================================================================\n");
 
         displayMenu(mainMenuOptions, menuCount);
         
@@ -32,12 +34,14 @@ int main() {
 
         switch(choice) {
             case 1:
+                addStudent(student, &studentCount, studentSize);
                 break;
             case 2:
                 break;
             case 3:
                 break;
             case 4:
+                displayList(student, studentCount);
                 break;
             case 5:
                 printf("Exiting Program...\n");
@@ -55,4 +59,42 @@ void displayMenu(char mainMenuOptions[][50], int size) {
     }
 
     return;
+}
+
+void addStudent(STUDENT stud[], int *studentCount, int maxStudent) {
+    if(*studentCount >= maxStudent) {
+        printf("Cannot add more students. Maximum threshold reached.\n");
+        return;
+    }
+
+    STUDENT *newStudent = &stud[*studentCount];
+
+    printf("Enter student name: ");
+    getchar();
+    gets(newStudent->name);
+
+    printf("Enter course: ");
+    gets(newStudent->course);
+
+    printf("Enter ID: ");
+    scanf("%d", &newStudent->idNumber);
+
+    printf("Enter year: ");
+    scanf("%d", &newStudent->year);
+
+    (*studentCount)++;
+}
+
+void displayList(STUDENT stud[], int studentCount) {
+    int i;
+
+    if(studentCount == 0) {
+        printf("No students are available for display.\n");
+        return;
+    }
+
+    printf("\n-----------------------------------------Student List-----------------------------------------\n");
+    for(i = 0; i < studentCount; i++) {
+        printf("[%d]\t%s - %s\t ID: %d\t Year: %d\n", i+1, stud[i].name, stud[i].course, stud[i].idNumber, stud[i].year);
+    }
 }
